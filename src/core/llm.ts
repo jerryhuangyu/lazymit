ed79b68762cf4357394d7a786697afc972e3c24d
jerryhuangyu <jerry0932619628@gmail.com>
2024-11-26 15:29:49 +0800

    🐛  fix: Update ai model names

--- src/core/llm.ts
+++ src/core/llm.ts
@@ -5,7 +5,7 @@ import type { IConfig } from "./config";
 export enum LLM_MODEL {
 	GEMINI_FLASH = "gemini-1.5-flash-latest",
 	GEMINI_PRO = "gemini-1.5-pro-latest",
-	GPT_4O_LATEST = "chatgpt-4o",
+	GPT_4O_LATEST = "chatgpt-4o-latest",
 	GPT_4O_MINI = "gpt-4o-mini",
 }
 
